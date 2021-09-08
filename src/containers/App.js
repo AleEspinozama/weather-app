06733357de2aed5379f6444aa290d3e2fc2164b4
AleEspinozama <espinozalezama@gmail.com>
2021-09-08 18:00:21 -0500

    git pages added

--- src/containers/App.js
+++ src/containers/App.js
@@ -38,7 +38,7 @@ function App() {
           };
           setCities(oldCities => [...oldCities, ciudad]);
         } else {
-          alert("Ciudad no encontrada");
+          alert("City not found");
         }
       });
   }
