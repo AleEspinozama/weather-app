7832ec33286980cf4ef058151ff837979ac1ba15
AleEspinozama <espinozalezama@gmail.com>
2021-09-10 12:34:25 -0500

    back to main from about when onSubmit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
 import s from './SearchBar.module.css';
+import { useHistory } from "react-router-dom";
 
 export default function SearchBar({onSearch}) {
   // acá va tu código
   const [city, setCity] = useState("");
-
+  let history = useHistory();
 
   return (
     <form onSubmit={(e) => {
       e.preventDefault();
       onSearch(city);
       setCity("");
+      history.push('/weather-app/');
+      
     }}>
           <input 
           className={s.input} 
